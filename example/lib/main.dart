6e909f213e29dd9f485b085697c31207856c1b09
Dobszai János <dobszai.janos@innobyte.hu>
2021-02-17 21:55:58 +0100

    scale + FractionallySizedBox

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_echarts/flutter_echarts.dart';
 import 'package:number_display/number_display.dart';
 
@@ -29,114 +30,138 @@ class MySuperWideChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      // width: 300,
+      child: FakeDevicePixelRatio(
+        ratio: 0.5,
         child: Echarts(
-      extraScript: '''
-        var colors = ['#5470C6', '#91CC75', '#EE6666'];
-        ''',
-      option: '''
-        {
-            color: colors,
-        
-            tooltip: {
-                trigger: 'axis',
-                axisPointer: {
-                    type: 'cross'
-                }
-            },
-            grid: {
-                right: '20%'
-            },
-            toolbox: {
-                feature: {
-                    dataView: {show: true, readOnly: false},
-                    restore: {show: true},
-                    saveAsImage: {show: true}
-                }
-            },
-            legend: {
-                data: ['蒸发量', '降水量', '平均温度']
-            },
-            xAxis: [
+          extraScript: '''
+                var colors = ['#5470C6', '#91CC75', '#EE6666'];
+                ''',
+          option: '''
                 {
-                    type: 'category',
-                    axisTick: {
-                        alignWithLabel: true
-                    },
-                    data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
-                }
-            ],
-            yAxis: [
-                {
-                    type: 'value',
-                    name: '蒸发量',
-                    min: 0,
-                    max: 250,
-                    position: 'right',
-                    axisLine: {
-                        show: true,
-                        lineStyle: {
-                            color: colors[0]
+                    color: colors,
+                
+                    tooltip: {
+                        trigger: 'axis',
+                        axisPointer: {
+                            type: 'cross'
                         }
                     },
-                    axisLabel: {
-                        formatter: '{value} ml'
-                    }
-                },
-                {
-                    type: 'value',
-                    name: '降水量',
-                    min: 0,
-                    max: 250,
-                    position: 'right',
-                    offset: 80,
-                    axisLine: {
-                        show: true,
-                        lineStyle: {
-                            color: colors[1]
+                    grid: {
+                        right: '20%'
+                    },
+                    toolbox: {
+                        feature: {
+                            dataView: {show: true, readOnly: false},
+                            restore: {show: true},
+                            saveAsImage: {show: true}
                         }
                     },
-                    axisLabel: {
-                        formatter: '{value} ml'
-                    }
-                },
-                {
-                    type: 'value',
-                    name: '温度',
-                    min: 0,
-                    max: 25,
-                    position: 'left',
-                    axisLine: {
-                        show: true,
-                        lineStyle: {
-                            color: colors[2]
-                        }
+                    legend: {
+                        data: ['蒸发量', '降水量', '平均温度']
                     },
-                    axisLabel: {
-                        formatter: '{value} °C'
-                    }
+                    xAxis: [
+                        {
+                            type: 'category',
+                            axisTick: {
+                                alignWithLabel: true
+                            },
+                            data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+                        }
+                    ],
+                    yAxis: [
+                        {
+                            type: 'value',
+                            name: '蒸发量',
+                            min: 0,
+                            max: 250,
+                            position: 'right',
+                            axisLine: {
+                                show: true,
+                                lineStyle: {
+                                    color: colors[0]
+                                }
+                            },
+                            axisLabel: {
+                                formatter: '{value} ml'
+                            }
+                        },
+                        {
+                            type: 'value',
+                            name: '降水量',
+                            min: 0,
+                            max: 250,
+                            position: 'right',
+                            offset: 80,
+                            axisLine: {
+                                show: true,
+                                lineStyle: {
+                                    color: colors[1]
+                                }
+                            },
+                            axisLabel: {
+                                formatter: '{value} ml'
+                            }
+                        },
+                        {
+                            type: 'value',
+                            name: '温度',
+                            min: 0,
+                            max: 25,
+                            position: 'left',
+                            axisLine: {
+                                show: true,
+                                lineStyle: {
+                                    color: colors[2]
+                                }
+                            },
+                            axisLabel: {
+                                formatter: '{value} °C'
+                            }
+                        }
+                    ],
+                    series: [
+                        {
+                            name: '蒸发量',
+                            type: 'bar',
+                            data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+                        },
+                        {
+                            name: '降水量',
+                            type: 'bar',
+                            yAxisIndex: 1,
+                            data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+                        },
+                        {
+                            name: '平均温度',
+                            type: 'line',
+                            yAxisIndex: 2,
+                            data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+                        }
+                    ]
                 }
-            ],
-            series: [
-                {
-                    name: '蒸发量',
-                    type: 'bar',
-                    data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
-                },
-                {
-                    name: '降水量',
-                    type: 'bar',
-                    yAxisIndex: 1,
-                    data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
-                },
-                {
-                    name: '平均温度',
-                    type: 'line',
-                    yAxisIndex: 2,
-                    data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
-                }
-            ]
-        }
-        ''',
-    ));
+                ''',
+        ),
+      ),
+    );
+  }
+}
+
+class FakeDevicePixelRatio extends StatelessWidget {
+  final double ratio;
+  final Widget child;
+
+  FakeDevicePixelRatio({this.ratio, this.child}) : assert(ratio != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.scale(
+      scale: ratio,
+      child: FractionallySizedBox(
+        widthFactor: 1 / ratio,
+        heightFactor: 1 / ratio,
+        child: child,
+      ),
+    );
   }
 }
